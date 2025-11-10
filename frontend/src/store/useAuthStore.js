7d430909a77e161abd63666a8a7fe7dfb8a7a2c0
Samarth Kolarkar <samarthkolarkar7777@gmail.com>
2025-11-10 15:50:02 +0530

    Project completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,8 @@ import { disconnect } from "mongoose";
 import {io} from "socket.io-client";
 
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
 
 export const useAuthStore = create((set,get)=>({
     authUser:null,
